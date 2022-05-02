8af1ffccaa6327398eca99be03f558a9130d357c
Laura Esther Correia Jeronimo <79860690+Laura-Jeronimo@users.noreply.github.com>
2022-05-02 09:21:59 -0300

    Add files via upload

---
+++ aula_25_04/exemplo.js
@@ -0,0 +1,3 @@
+// teste com plugin de js
+console.log('TESTE TESTE TESTE')
+// só apertar f8 pra rodar ae 
---
+++ aula_25_04/exemplo2.js
@@ -0,0 +1,7 @@
+var vel = 59
+console.log(`A velocidade do carro é de ${vel} Km/h`)
+if(vel>=60){
+    console.log('Você ultrapassou o limite de velocidade')
+    console.log('Você será multado')
+}
+console.log('Dirija com segurança')
